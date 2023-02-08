0b2feea7c271a30f805b5776bf79cc4c240d2032
Aviral-PC <aviral17@hotmail.com>
2023-02-08 15:33:51 +0530

    PDFs generated with total

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fpdf import FPDF
 #get filepaths of excel files, and store in list
 invoice_list=glob.glob("Invoices/*.xlsx")
 
-#Iterate through filepath
+#Iterate through each filepath
 for item in invoice_list:
     #create PDF invoice
     pdf=FPDF(orientation='P',unit='mm',format="A4")
@@ -17,30 +17,37 @@ for item in invoice_list:
     pdf.ln(3)
     pdf.cell(w=0, h=3, txt=f"Date - {invoice_date[:-5]}", border=0, ln=1, align="L")
     pdf.ln(3)
-    #Titles
+    #Column Headers
     pdf.set_font(family="Times", style="B", size=12)
-    pdf.cell(w=30, h=8, txt='Product_id',align="C")
-    pdf.cell(w=60, h=8, txt='Product_name',align="C")
-    pdf.cell(w=40, h=8, txt='Amount_purchased',align="C")
-    pdf.cell(w=30, h=8, txt='Price_per_unit',align="C")
-    pdf.cell(w=30, h=8, txt='Total_price',align="C")
+    pdf.cell(w=30, h=8, txt='Product_id',align="C",border=1)
+    pdf.cell(w=60, h=8, txt='Product_name',align="C",border=1)
+    pdf.cell(w=40, h=8, txt='Amount_purchased',align="C",border=1)
+    pdf.cell(w=30, h=8, txt='Price_per_unit',align="C",border=1)
+    pdf.cell(w=30, h=8, txt='Total_price',align="C",border=1)
     pdf.ln(10)
-
-    filename_output_pdf = item.split('\\')[1][:-5]
 
     #Read Excel and convert to pdf
     df = pd.read_excel(item, 'Sheet 1')
     total = 0
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=12)
-        pdf.cell(w=30,h=8,txt=str(row['product_id']),align="C")
-        pdf.cell(w=60, h=8, txt=row['product_name'],align="C")
-        pdf.cell(w=40, h=8, txt=str(row['amount_purchased']),align="C")
-        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']),align="C")
-        pdf.cell(w=30, h=8, txt=str(row['total_price']),align="C")
+        pdf.cell(w=30,h=8,txt=str(row['product_id']),align="C",border=1)
+        pdf.cell(w=60, h=8, txt=row['product_name'],align="C",border=1)
+        pdf.cell(w=40, h=8, txt=str(row['amount_purchased']),align="C",border=1)
+        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']),align="C",border=1)
+        pdf.cell(w=30, h=8, txt=str(row['total_price']),align="C",border=1)
         pdf.ln(10)
-        #print(row['total_price'])
-        #total = total + float(row['total_price'])
+        total = total + float(row['total_price'])
 
+    #Write the total
+    pdf.cell(w=30, h=8, txt='', align="C")
+    pdf.cell(w=60, h=8, txt='', align="C")
+    pdf.cell(w=40, h=8, txt='', align="C")
+    pdf.set_font(family="Times", size=12,style="B")
+    pdf.cell(w=30, h=8, txt='TOTAL', align="C",border=1)
+    pdf.set_font(family="Times", size=12)
+    pdf.cell(w=30, h=8, txt=str(total), align="C",border=1)
+
+    filename_output_pdf = item.split('\\')[1][:-5]
     pdf.output(f"PDFs/{filename_output_pdf}.pdf")
 
